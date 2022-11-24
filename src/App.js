07f361e995b78dc2b7091db7b1c3b2b8f534d88f
Miriam Fernandez <mirindaww@hotmail.com>
2022-11-24 07:01:27 +0100

    Ejercicio 4

--- src/App.js
+++ src/App.js
@@ -1,46 +1,66 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Checkbox from './components/Checkbox';
 import Panel from './components/Panel';
 import Boton from './components/Boton';
 
 function App() {
+  
+  // const [total, setTotal] = useState(0);
+  // let [webChecked, setWebChecked] = useState(false);
+  // let [seoChecked, setSeoChecked] = useState(false);
+  // let [adsChecked, setAdsChecked] = useState(false);
+  // let [paginas, setPaginas] = useState(0);
+  // let [idiomas, setIdiomas] = useState(0);
 
-  const [total, setTotal] = useState(0);
-  const [isChecked, setChecked] = useState(false);
-  let [paginas, setPaginas] = useState(0);
-  let [idiomas, setIdiomas] = useState(0);
+  const [total, setTotal] = useState(parseInt(localStorage.getItem('total')) ? parseInt(localStorage.getItem('total')) : 0);
+  let [webChecked, setWebChecked] = useState(localStorage.getItem('webChecked') ? (localStorage.getItem('webChecked')) : false);
+  let [seoChecked, setSeoChecked] = useState(localStorage.getItem('seoChecked') ? (localStorage.getItem('seoChecked')) : false);
+  let [adsChecked, setAdsChecked] = useState(localStorage.getItem('adsChecked') ? (localStorage.getItem('adsChecked')) : false);
+  let [paginas, setPaginas] = useState(JSON.parse(localStorage.getItem('paginas')) ? JSON.parse(localStorage.getItem('paginas')) : 0);
+  let [idiomas, setIdiomas] = useState(JSON.parse(localStorage.getItem('idiomas')) ? JSON.parse(localStorage.getItem('idiomas')) : 0);
+
   let totalExtras = (paginas + idiomas) * 30;
+
+  useEffect(()=>{
+    try{
+      localStorage.setItem('total', total);
+      localStorage.setItem('paginas', paginas);
+      localStorage.setItem('idiomas', idiomas);
+      localStorage.setItem('webChecked', webChecked);
+      localStorage.setItem('seoChecked', seoChecked);
+      localStorage.setItem('adsChecked', adsChecked);
+    } catch (error){
+      console.error(error);
+    }
+  },[total, paginas, idiomas, webChecked, seoChecked, adsChecked]);
   
 
   const handleInputChange = (event) => {
     const {name, value, checked} = event.target;
-    console.log(name, value, checked);
+    //console.log(name, value, checked);
 
-    if(checked){
-      setTotal(total + parseInt(value));
-    }else{
-      setTotal(total - parseInt(value));
-    }
-
+    checked ? setTotal(total + parseInt(value)) : setTotal(total - parseInt(value))
+    
+    //Si el onChange es en Web
     if (name == 'web'){
-      //Y está seleccionado sumo precio
-      if (checked == true){
-        setChecked(true);
-      }else{
-        setChecked(false);
-      }
+      checked ? setWebChecked( webChecked = true) : setWebChecked( webChecked = false) ;
     }
-
+    
+    //Si el onChange es en Seo
+    if (name == 'seo'){
+      checked ? setSeoChecked( seoChecked = true) :setSeoChecked( seoChecked = false)
+    }
+    
+    //Si el onChange es en Ads
+    if (name == 'ads'){
+      checked ? setAdsChecked( adsChecked = true) : setAdsChecked( adsChecked= false)
+    }
   }
+  
 
   const handleNumberChange = (event) => {
     const {name, value} = event.target;
-    console.log(name, value);
-    if (name == 'paginas'){
-      setPaginas( paginas = Number(value) );
-    }else{
-      setIdiomas( idiomas = Number(value) );
-    }
+    name == 'paginas' ? setPaginas( paginas = Number(value) ) : setIdiomas( idiomas = Number(value) )
   }
 
   const handleClick = (event) =>{
@@ -65,15 +85,15 @@ function App() {
     }
   }
 
-  console.log('Paginas '+paginas, 'Idiomas '+ idiomas);
+  
   return (
     <div className="App">
       <div className='container'>
       <h3 className='mt-5 text-start'>¿Qué quieres hacer?</h3>
         <form action="" className="row text-start">
-          <Checkbox name="web" id="web" value={500} onChange={handleInputChange} label="Una página web (500€)" ></Checkbox>
+          <Checkbox name="web" id="web" value={500} onChange={handleInputChange} label="Una página web (500€)" checked={eval(webChecked)} ></Checkbox>
 
-          { (isChecked) && <Panel>
+          { (webChecked) && <Panel>
             <div>
               <label className="me-2" htmlFor="paginas">Número de páginas</label>
               <Boton name="masPaginas" onClick={handleClick}>+</Boton>
@@ -88,8 +108,8 @@ function App() {
             </div>
           </Panel>}
 
-          <Checkbox name="seo" id="seo" value={300} onChange={handleInputChange} label="Una consultoría SEO (300€)"></Checkbox>
-          <Checkbox name="ads" id="ads" value={200} onChange={handleInputChange} label="Una campaña de Google Ads (200€)"></Checkbox>
+          <Checkbox name="seo" id="seo" value={300} onChange={handleInputChange} label="Una consultoría SEO (300€)" checked={eval(seoChecked)} ></Checkbox>
+          <Checkbox name="ads" id="ads" value={200} onChange={handleInputChange} label="Una campaña de Google Ads (200€)" checked={eval(adsChecked)}></Checkbox>
         </form>
         <div className="col-sm-12 text-start">
           <p>Precio: {total+totalExtras} </p>
--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,7 +4,7 @@ const Checkbox = (props) => {
 
     return (
       <label>
-        <input type="checkbox" className="form-check-input" name={props.name} id={props.id} value={props.value} onChange={props.onChange} />
+        <input type="checkbox" className="form-check-input" name={props.name} id={props.id} value={props.value} onChange={props.onChange} checked={props.checked} />
           {props.label}
       </label>
       
