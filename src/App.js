6ebad326fb92627ec20b69ce5c6510f41332335d
Miriam Fernandez <mirindaww@hotmail.com>
2022-11-24 02:13:02 +0100

    Ejercicio 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import React, {useState} from 'react';
 import Checkbox from './components/Checkbox';
+import Panel from './components/Panel';
 
 function App() {
 
   const [total, setTotal] = useState(0);
+  const [isChecked, setChecked] = useState(false);
+  let [paginas, setPaginas] = useState(0);
+  let [idiomas, setIdiomas] = useState(0);
+  
+  let totalExtras = (paginas + idiomas) * 30;
+  console.log(totalExtras);
 
   const handleInputChange = (event) => {
     const {name, value, checked} = event.target;
@@ -16,6 +23,27 @@ function App() {
       setTotal(total - parseInt(value));
     }
 
+    if (name == 'web'){
+      //Y está seleccionado sumo precio
+      if (checked == true){
+        setChecked(true);
+      }else{
+        setChecked(false);
+      }
+    }
+
+  }
+
+  const handleNumberChange = (event) => {
+    const {name, value} = event.target;
+    console.log(name, value);
+    if (name == 'paginas'){
+      setPaginas( paginas = Number(value) );
+    }else{
+      setIdiomas( idiomas = Number(value) );
+    }
+        
+    
   }
 
   return (
@@ -24,11 +52,23 @@ function App() {
       <h3 className='mt-5 text-start'>¿Qué quieres hacer?</h3>
         <form action="" className="row text-start">
           <Checkbox name="web" id="web" value={500} onChange={handleInputChange} label="Una página web (500€)" ></Checkbox>
+
+          { (isChecked) && <Panel>
+            <div>
+              <label className="me-2" htmlFor="paginas">Número de páginas</label>
+              <input className="form-control d-inline w-25" name="paginas" id="paginas" type="number" min={0} onChange={handleNumberChange} />
+            </div>
+            <div>
+              <label className="me-2" htmlFor="idiomas">Número de idiomas</label>
+              <input className="form-control d-inline w-25 mt-2" name="idiomas" id="idiomas" type="number" min={0} onChange={handleNumberChange} />
+            </div>
+          </Panel>}
+
           <Checkbox name="seo" id="seo" value={300} onChange={handleInputChange} label="Una consultoría SEO (300€)"></Checkbox>
           <Checkbox name="ads" id="ads" value={200} onChange={handleInputChange} label="Una campaña de Google Ads (200€)"></Checkbox>
         </form>
         <div className="col-sm-12 text-start">
-          <p>Precio: {total} </p>
+          <p>Precio: {total+totalExtras} </p>
         </div>
       </div>
     </div>
---
+++ src/components/Panel.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Panel = styled.div`
+  width: auto;
+  border: 0.1rem solid #000;
+  border-radius: 0.25rem;
+  margin: 0.5rem;
+  padding: 0.5rem;
+  
+  
+`;
+export default Panel;
